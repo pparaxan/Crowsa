4cf8f8d9a070c757c1ad4f4e0fb908d72fd6f127
WilliamAnimate <nyameowmeow@duck.com>
2024-12-07 00:45:23 -0500

    docs: more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+//! Quarks are a very small thing.
+//!
+//! Quark is a diy native web UI library designed to help you create small, fast and responsive
+//! desktop apps.
+//! It provides multiple advantages over other similar projects, like [Electron]:
+//! - Miniscule (178128 bytes/178 kb)
+//! - Extremely fast rapid edit-debug-run cycles due to low dependency count
+//! - No npm required to build 🎉!
+//!
+//! [Electron]: https://www.electronjs.org/
+
+#![warn(missing_docs)]
+
 pub mod cli;
 pub mod config;
 pub mod error;
@@ -16,6 +29,24 @@ pub struct Quark {
 }
 
 impl Quark {
+    /// Creates a `Quark` type from a `QuarkConfig`
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use libquark::prelude::*;
+    /// # fn main() -> Result<(), QuarkError> {
+    /// let config = QuarkConfig::new().title("Quark");
+    /// let quark = Quark::new(config)?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// Most applications utilizing Quark will often call the `.run` method right after:
+    ///
+    /// ```rust,ignore
+    /// quark.run();
+    /// ```
     pub fn new(config: QuarkConfig) -> Result<Self, QuarkError> {
         let args = cli::parse_args();
 
@@ -52,11 +83,37 @@ impl Quark {
         self.webview.bind(name, handler);
     }
 
-    pub fn eval(&mut self, js: &str) {
+    /// Evaluates JavaScript code on the frontend web application
+    ///
+    /// This can be used to execute code on the frontend web application at any time in the
+    /// backend.
+    ///
+    /// # Example
+    ///
+    /// ```rust,ignore
+    /// quark.eval("alert('hello!')");
+    /// ```
+    pub fn eval(&self, js: &str) {
         self.webview.eval(js);
     }
 
-    pub fn run(mut self) {
+    /// Starts the quark application
+    ///
+    /// This will initialize the webview and show a window. This blocks the thread until Quark is
+    /// closed.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use libquark::prelude::*;
+    /// # fn main() -> Result<(), QuarkError> {
+    /// let config = QuarkConfig::new().title("Quark");
+    /// let quark = Quark::new(config)?;
+    /// quark.run();
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub fn run(&mut self) {
         self.webview.run();
     }
 }
